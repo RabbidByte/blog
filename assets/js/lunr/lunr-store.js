30d9f9d2ea43e97d41924213daabbdf8367e611b
RabbidByte <48965238+RabbidByte@users.noreply.github.com>
2019-03-27 21:21:22 -0600

    Start - added files
    added all the initial files

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Mobile Hacking",
+        "excerpt":"I have always loved wireless security. It is just such an easy target. The only issue that I have had is that I was restricted to carrying around a laptop and I just didn’t like the idea of having to operate the software while I was out. So I came...","categories": ["Blog","Misc"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/Mobile-Hacking.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"},{
+        "title": "GFI LanGuard 2012 Priviledge Escalation",
+        "excerpt":"Download https://github.com/RabbidByte/GFI2012-PrivilegeEscalation What is GFI LanGuard 2012? GFI LanGuard 2012 http://www.gfi.com/network-security-vulnerability-scanner is a software package that competes with Microsoft SCCM. This software is placed in a network environment to inventory hardware, software, perform basic vulnerability assessment, and manage the software packages on the computers within the network. At the time...","categories": ["Blog","Tools"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/GFI-LanGuard-2012-Priviledge-Escalation.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"},{
+        "title": "TransHex Server and Client",
+        "excerpt":"Summary We have all faced the problem of transferring “malicious” or binary files through corporate firewalls or proxies when doing penetration tests. Previously the work around was quite easy. FTP, SSL, and SSH tunneling (the list goes on) provided a quick and easy way to bring whatever tools a penetration...","categories": ["Blog","Tools"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/TransHex-Server-and-Client.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"},{
+        "title": "Transfer Any Binary into a Protected Network",
+        "excerpt":"Summary Well this is another old trick that still works today. It still gets past all edge security and antivirus … well that is until you pull the executable out again. I find myself using this trick when I need to bring penetration testing tools into a network. Most of...","categories": ["Blog","Misc"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/Transfer-Any-Binary-into-a-Protected-Network.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"},{
+        "title": "HackRF: The easy way",
+        "excerpt":"The Story Recently many different radio hacks (mousejack, drone hijacking) have hit the internet. This has spurred my interest in analogoue and digital radio. After some bindge shopping on the internet and a few days wait I have had some very nice toys show up. Once the new electronics smell...","categories": ["Blog","Misc"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/HackRF-The-easy-way.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"},{
+        "title": "AWS S3: Misconfiguration, Discovery, and Abuse",
+        "excerpt":"Summary Recent news of the Verizon data leak http://www.darkreading.com/cloud/verizon-suffers-cloud-data-leak-exposing-data-on-millions-of-customers/ and a similar scenario concerning Dow Jones http://www.darkreading.com/cloud/dow-jones-data-leak-results-from-amazon-aws-configuration-error/ prompted me to do a little “poking around”. The basic misconfiguration in these cases was that the administrators of the AWS S3 buckets permitted read access to “Everyone” from anywhere. So really all...","categories": ["Blog","Misc"],
+        "tags": [],
+        "url": "http://192.168.1.30:4000/AWS-S3-Misconfiguration-Discovery-and-Abuse.html",
+        "teaser":"http://192.168.1.30:4000/assets/images/500x300.png"}]
